c0e078527dec3b26524e9e2957eb006c7913ffc3
xiaoyang <744688054@qq.com>
2018-12-12 19:12:31 +0800

    yyy

--- dev-constants.js
+++ dev-constants.js
@@ -5,7 +5,7 @@ module.exports = {
       koa2: 'http://api.koa2.bananayc.com'
     },
     dev: {
-      java: 'http://localhost:8080',
+      java: 'https://interface.miniones.cn',
       koa2: 'http://api.koa2.bananayc.com'
         //java: 'https://interface.miniones.cn',
         //koa2: 'http://api.koa2.dev.bananayc.com'
