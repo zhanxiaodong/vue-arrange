1f0efb20f9341ceec345047851ef8c865d0d4698
zxd <103258469@qq.com>
2019-01-16 17:41:04 +0800

    zxd

--- dev-constants.js
+++ dev-constants.js
@@ -6,15 +6,9 @@ module.exports = {
 
     },
     dev: {
-<<<<<<< HEAD
-  //java: 'http://192.168.0.22:8080',
-   //koa2: 'http://api.koa2.bananayc.com'
-     java: 'https://interface.miniones.cn',
-=======
 //    java: 'http://localhost:8080',
 //    koa2: 'http://api.koa2.bananayc.com'
       java: 'https://interface.miniones.cn',
->>>>>>> fd7bfd0b81c70922bb1c119c7178d347be97f30f
       koa2: 'http://api.koa2.dev.bananayc.com'
     }
   }
